5135a4eb092f33dd6b822a6f3704fbf1551b93fd
Yohannes Alemu <alemuyohannes960@gmail.com>
2025-08-30 21:51:14 +0300

    a

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: '
-https://covenant-reformed-ministry-ethiopia.onrender.com', // This allows requests from any domain https://covenant-reformed-ministry-ethiopia.onrender.com
+  origin: 'https://covenant-reformed-ministry-ethiopia.onrender.com', // This allows requests from any domain https://covenant-reformed-ministry-ethiopia.onrender.com
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
 };
